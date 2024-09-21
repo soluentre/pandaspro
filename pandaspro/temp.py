9a11c239fde3af1b1f9441dacbf9cc208864a026
Shiyao at WB <swang12@worldbankgroup.org>
2024-09-21 16:39:07 -0400

    minor

--- pandaspro/temp.py
+++ pandaspro/temp.py
@@ -1,90 +1,84 @@
-import pandas as pd
-import pandaspro as cpd
+from pandaspro.core.tools.corder import corder
 
 
-def unify_to_list(para):
-    if isinstance(para, str | int):
+def unify_to_list(para: int | str | list):
+    if isinstance(para, int | str):
         para_list = [para]
-    if isinstance(para, list):
+    elif isinstance(para, list):
         para_list = para
+    else:
+        raise ValueError('Incorrect type of argument [para]: only supports int/str/list')
     return para_list
 
 
-class Spliter:
-    def __init__(self, df: pd.DataFrame | cpd.FramePro):
-        self.df = cpd.FramePro(df)
-        self.split_result = None
+class cpdConsecGrouper:
+    def __init__(
+            self,
+            df,
+            group_by,
+    ):
+        self.df = df
+        self.df_grouped = None
+
+        # Split Section
+        if isinstance(group_by, str):
+            self.group_by = [group_by]
+        elif isinstance(group_by, list):
+            self.group_by = group_by
+        else:
+            raise ValueError('Incorrect type for group_by, can only be str/list')
 
     # noinspection PyShadowingNames
-    def split(self,
-              sort_by: str | list = None,
-              split_by: str | list = None,
-              ):
+    def group(self):
+        self.df_grouped = self.df.copy()
+        self.df_grouped['group_num'] = (self.df_grouped[self.group_by] != self.df_grouped[self.group_by].shift()).any(axis=1).cumsum()
+        self.df_grouped['group_id'] = self.df_grouped[['group_num'] + self.group_by].astype(str).agg('-'.join, axis=1)
+        self.df_grouped = corder(self.df_grouped, ['group_num', 'group_id'])
+        return self.df_grouped
 
-        if sort_by:
-            df_sorted = self.df.sort_values(by=sort_by).reset_index(drop=True)
-        else:
-            df_sorted = self.df
-
-        if split_by:
-            if isinstance(split_by, str):
-                split_by_list = [split_by]
-            if isinstance(split_by, list):
-                split_by_list = split_by
-
-            df_sorted['group_num'] = (df_sorted[split_by_list] != df_sorted[split_by_list].shift()).any(axis=1).cumsum()
-            df_sorted['group_id'] = df_sorted[['group_num'] + split_by_list].astype(str).agg('-'.join, axis=1)
-            df_sorted = df_sorted.corder(['group_num', 'group_id'])
-
-            self.split_result = df_sorted
-            return df_sorted
-        else:
-            return df_sorted
-
-    def smart_group(self,
-                    sort_by: str | list = None,
-                    split_by: str | list = None,
-                    top: str | list = None,
-                    bottom: str | list = None,
-                    pick_group: int | str | list = None
-                    ):
-        grouped_df = self.split(sort_by=sort_by, split_by=split_by)
-
+    def extract(
+            self,
+            value_at_top: str | list = None,
+            value_at_bottom: str | list = None,
+    ):
+        grouped_df = self.group()
         group_id_dict = {'group_num': 'first', 'group_id': 'first'}
-        split_by_dict = {var: 'first' for var in unify_to_list(split_by)}
-        top_dict = {var: 'first' for var in unify_to_list(top)}
-        bottom_dict = {var: 'last' for var in unify_to_list(bottom)}
+        group_by_dict = {var: 'first' for var in unify_to_list(self.group_by)}
+        top_dict = {var: 'first' for var in unify_to_list(value_at_top)}
+        bottom_dict = {var: 'last' for var in unify_to_list(value_at_bottom)}
 
         agg_dict = {}
-        for d in [group_id_dict, split_by_dict, top_dict, bottom_dict]:
+        for d in [group_id_dict, group_by_dict, top_dict, bottom_dict]:
             for key, value in d.items():
                 if key not in agg_dict:
                     agg_dict[key] = []
                 agg_dict[key].append(value)
         print(agg_dict)
 
-        grouped_df = grouped_df.groupby('group_id').agg(agg_dict).reset_index(drop=True)
-        grouped_df.columns = [col[0] if col[0] in unify_to_list(['group_num', 'group_id'] + split_by)
+        grouped_df = grouped_df.groupby('group_num').agg(agg_dict).reset_index(drop=True)
+        grouped_df.columns = [col[0] if col[0] in unify_to_list(['group_num', 'group_id'] + self.group_by)
                               else '_'.join(filter(None, col)).rstrip('_')
                               for col in grouped_df.columns]
         grouped_df.columns = [col.replace('first', 'top').replace('last', 'bottom') for col in grouped_df.columns]
 
-        if pick_group:
-            return grouped_df[grouped_df['group_num'].isin(unify_to_list(pick_group)) | grouped_df['group_id'].isin(unify_to_list(pick_group))]
-
-        else:
-            return grouped_df
+        return grouped_df
 
 
 if __name__ == '__main__':
-    mona = cpd.pwread(
-        r'C:\Users\xli7\OneDrive - International Monetary Fund (PRD)\Databases\MONA\Description\Description_20240328.xlsx')[
-        0]
+    # mona = cpd.pwread(
+    #     r'C:\Users\xli7\OneDrive - International Monetary Fund (PRD)\Databases\MONA\Description\Description_20240328.xlsx')[
+    #     0]
+    #
+    # a = Spliter(mona)
+    # b = a.split(split_by=['arrangement_number', 'review_sequence'])
+    # c = a.smart_group(sort_by=['arrangement_number', 'board_action_date'],
+    #                   split_by=['arrangement_number', 'review_sequence'],
+    #                   top=['review_type', 'board_action_date'],
+    #                   bottom='review_type',
+    #                   pick_group=[1, '2-501-BLANK'])
+    from wbhrdata import *
+    data = psoftjob()
+    r = cpdConsecGrouper(data, ['upi', 'location']).extract(value_at_top=['eff_dt'], value_at_bottom='eff_dt')
+    r = r.sort_values('group_num')
+    # b = a.group()
 
-    a = Spliter(mona)
-    b = a.split(split_by=['arrangement_number', 'review_sequence'])
-    c = a.smart_group(sort_by=['arrangement_number', 'board_action_date'],
-                      split_by=['arrangement_number', 'review_sequence'],
-                      top=['review_type', 'board_action_date'],
-                      bottom='review_type',
-                      pick_group=[1, '2-501-BLANK'])

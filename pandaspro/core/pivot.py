4acffdaa7fefef1e25eee6261719ef501368098e
Shiyao at WB <swang12@worldbankgroup.org>
2024-09-30 21:21:18 -0400

    pivotpro design

--- pandaspro/core/pivot.py
+++ pandaspro/core/pivot.py
@@ -7,3 +7,31 @@ pivot_table will return a pd.DataFrame object, let's first inherit the DataFrame
     4. we need be able to claim a dataframe as this class easily: classname(df, '0-y-y-0)
 
 '''
+# mapper_nb = {..para..} + {..position..}
+#
+# i_give = {'top1': True}
+# i_give_parse = {
+#     'id1 - deptGA': {
+#         'top1': True
+#     },
+#     'id2 - deptGB': {
+#         'top1': True
+#     }
+#     ...
+# }
+# from_structure_parse = {
+#     'id1 - deptGA': {
+#         'top1': True,
+#     },
+#     'id2 - deptGB': {
+#         'top1': False,
+#     }
+# }
+#
+# for id in grpn_1:  ## id == deptGA is a string not a numeric
+#     df_basic = [df.inlist('grpn_1', id)]
+#     df_tops = []
+#     for i in range(n-1, 1):
+#         top_row = df.pivot(...n-1...).inlist('grpn-1', id)
+#         if mapper_nb[id]['top'][n-1]['Yes_or_No']:  ## this is mapper_nb
+#             df_tops = df_tops.append(top_row)

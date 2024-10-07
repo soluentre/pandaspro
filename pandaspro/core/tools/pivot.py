c3a456f06f2ec28856d7bbbf4948b4b835f3e3a2
Shelley.Li.imf <lixueqi1213@gmail.com>
2024-10-06 22:35:23 -0400

    pivot pro

--- pandaspro/core/tools/pivot.py
+++ pandaspro/core/tools/pivot.py
@@ -9,8 +9,8 @@ from pandaspro.sample_df import df
 #     if 'aggfunc' in kwargs.keys():
 #     pivot_table(**kwargs)
 
+# noinspection PyShadowingNames
 def gen_all_level_index(df, columns_list):
-
     groupid_full = df[columns_list].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
     unique_groupid_full = sorted(groupid_full.unique())
     current_list = unique_groupid_full
@@ -40,7 +40,7 @@ def gen_all_level_index(df, columns_list):
 
     final_groupids = ['Grand Total_top'] + current_list + ['Grand Total_bottom']
 
-    return pd.DataFrame({"groupid": final_groupids})
+    return pd.DataFrame({'id': final_groupids})
 
 
 def gen_multi_level_pivot(
@@ -49,31 +49,37 @@ def gen_multi_level_pivot(
         columns,
         values,
         aggfunc,
-        levels_for_pivot: list
+        subtotal: dict
 ):
     if isinstance(index, str):
         index = [index]
 
     pivot_main = df.pivot_table(index=index,
-                              columns=columns,
-                              values=values,
-                              aggfunc=aggfunc).reset_index()
-    pivot_main['id'] = df[columns_list].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
+                                columns=columns,
+                                values=values,
+                                aggfunc=aggfunc).reset_index()
+    pivot_main['id'] = df[index].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
 
     pivot_subtotal = pd.DataFrame()
-    for level in levels_for_pivot:
-        print(level)
-        addtional_level_index = index[:index.index(level)+1]
+
+    for level, position in subtotal.items():
+        addtional_level_index = index[:index.index(level) + 1]
         pivot_additonal_level = df.pivot_table(index=addtional_level_index,
-                              columns=columns,
-                              values=values,
-                              aggfunc=aggfunc).reset_index()
-        pivot_additonal_level['id'] = df[addtional_level_index].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
+                                               columns=columns,
+                                               values=values,
+                                               aggfunc=aggfunc).reset_index()
+        pivot_additonal_level['id'] = df[addtional_level_index].apply(lambda row: '_'.join(row.values.astype(str)),
+                                                                      axis=1)
+        if position == 'top':
+            pivot_additonal_level['id'] = pivot_additonal_level['id'] + '_top'
+        if position == 'bottom':
+            pivot_additonal_level['id'] = pivot_additonal_level['id'] + '_bottom'
         pivot_subtotal = pd.concat([pivot_subtotal, pivot_additonal_level])
 
     result_df = pd.concat([pivot_main, pivot_subtotal])
 
     return result_df
+
 
 def gen_pivot_pro(
         df,
@@ -81,25 +87,24 @@ def gen_pivot_pro(
         columns,
         values,
         aggfunc,
-        subtotal_level: dict
+        subtotal: dict
 ):
-    all_level_index
+    all_level_index = gen_all_level_index(df, index)
+    multi_level_pivot = gen_multi_level_pivot(df, index, columns, values, aggfunc, subtotal)
+    pivot_pro = all_level_index.merge(multi_level_pivot, on='id', how='left')
+    pivot_pro = pivot_pro.dropna(subset=pivot_pro.columns.difference(['id']), how='all')
+    return pivot_pro
 
 
 if __name__ == '__main__':
-    final_df = gen_multi_level_pivot(df,
-                                     index=['department', 'grade'],
-                                     columns='gender',
-                                     values='salary',
-                                     aggfunc='count',
-                                     subtotal={'department': 'bottom'})
-
-    # 根据描述的逻辑生成所需的 groupid 列表
-
-    # 先定义给定的列列表
-    columns_list = ["department", 'unit']
-
-    # 调用函数生成符合层级关系的groupid DataFrame
-    all_level_index = gen_all_level_index(df, columns_list)
-
-
+    all_index = gen_all_level_index(df, ['department', 'grade'])
+    all_pivot = gen_multi_level_pivot(df, index=['department', 'grade'],
+                             columns='gender',
+                             values='salary',
+                             aggfunc='count',
+                             subtotal={'department': 'bottom'})
+    final_df = gen_pivot_pro(df, index=['department', 'grade'],
+                             columns='gender',
+                             values='salary',
+                             aggfunc='count',
+                             subtotal={'department': 'bottom'})

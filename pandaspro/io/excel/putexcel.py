0b94202dede592de911e4add7a74af754af9c491
Shiyao at WB <swang12@worldbankgroup.org>
2025-02-12 19:21:33 -0500

    merge_up and merge_add_rows

--- pandaspro/io/excel/putexcel.py
+++ pandaspro/io/excel/putexcel.py
@@ -615,10 +615,12 @@ class PutxlSet:
                                     updated_range_cells = CellPro(range_cells).offset(-1, 0).resize(2, 1).cell
                                     self.logger.info(f"\t\t[merge_up] is detected, this is for header style, the updated range is **{updated_range_cells}**")
                                     RangeOperator(self.ws.range(updated_range_cells)).format(**format_kwargs, debug=debug)
+                                    RangeOperator(self.ws.range(updated_range_cells)).format(**format_kwargs)
                                 elif additional_header_rule == 'merge_add_top':
                                     updated_range_cells = CellPro(range_cells).offset(-1, 0).cell
                                     self.logger.info(f"\t\t[merge_add_top] is detected, this is for header style, the updated range is **{updated_range_cells}**")
                                     RangeOperator(self.ws.range(updated_range_cells)).format(**format_kwargs, debug=debug)
+                                    RangeOperator(self.ws.range(updated_range_cells)).format(**format_kwargs)
                             else:
                                 RangeOperator(self.ws.range(range_cells)).format(**format_kwargs, debug=debug)
                         elif range_cells == '' or range_cells == 'N/A':

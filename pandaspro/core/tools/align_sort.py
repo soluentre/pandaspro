50483c41082d82f1a59ff73aadd02d0c0f669652
sw1016 <sw1016@georgetown.edu>
2026-01-14 14:05:27 -0500

    refine the align_sort: only keep relevant

--- pandaspro/core/tools/align_sort.py
+++ pandaspro/core/tools/align_sort.py
@@ -11,19 +11,21 @@ def align_and_sort_by_order(
 ):
     """
     按照指定的 order 对某列进行排序，并可选地重命名该列。
+    只保留 order 中存在的值，其他值会被过滤掉。
     
     :param data: 要排序的 DataFrame
     :param input_col: 要排序的列名
-    :param order: 定义排序顺序的列表
+    :param order: 定义排序顺序的列表，只保留列表中的值
     :param output_col: 输出列名（可选），如果提供则重命名列，否则保持原列名
     :param inplace: 是否在原地修改 DataFrame（默认: False）
     
     :return: 排序后的 DataFrame，如果 inplace=True 则返回 None
     
     Example:
-        >>> df = pd.DataFrame({'grade_fo': ['B', 'A', 'C'], 'value': [1, 2, 3]})
+        >>> df = pd.DataFrame({'grade_fo': ['B', 'A', 'C', 'D'], 'value': [1, 2, 3, 4]})
         >>> order_staff = ['A', 'B', 'C']
         >>> result = align_and_sort_by_order(df, input_col='grade_fo', order=order_staff, output_col='grade')
+        >>> # 结果只包含 A, B, C，D 被过滤掉
     """
     # 检查列是否存在
     if input_col in data.columns:
@@ -37,21 +39,19 @@ def align_and_sort_by_order(
     if output_col is None:
         output_col = input_col
     
-    # 获取所有唯一值
-    unique_values = list(data[input_col].dropna().unique())
-    
-    # 补全 order 列表（将 order 中没有但数据中存在的值追加到末尾）
-    provided_order = [x for x in order if x in unique_values]
-    missing_values = [x for x in unique_values if x not in order]
-    full_orderlist = provided_order + missing_values
-    
-    # 创建分类类型
-    cat_type = pd.CategoricalDtype(categories=full_orderlist, ordered=True)
-    
-    # 创建临时排序列
-    data['__cpd_align_sort'] = data[input_col].astype(cat_type)
+    # 只保留 order 中存在的值
+    full_orderlist = [x for x in order if x in data[input_col].values]
     
     if inplace:
+        # 先过滤只保留 order 中的值
+        mask = data[input_col].isin(order)
+        rows_to_drop = data.index[~mask]
+        data.drop(rows_to_drop, inplace=True)
+        
+        # 创建分类类型
+        cat_type = pd.CategoricalDtype(categories=full_orderlist, ordered=True)
+        data['__cpd_align_sort'] = data[input_col].astype(cat_type)
+        
         # 原地排序
         data.sort_values(by='__cpd_align_sort', inplace=True, kind='mergesort')
         
@@ -66,8 +66,16 @@ def align_and_sort_by_order(
         if set(data.index.names) <= set(data.columns):
             data.drop([name for name in data.index.names if name in data.columns], axis=1, inplace=True)
     else:
-        # 返回新的 DataFrame
-        result = data.sort_values(by='__cpd_align_sort', kind='mergesort').copy()
+        # 先过滤只保留 order 中的值
+        mask = data[input_col].isin(order)
+        result = data[mask].copy()
+        
+        # 创建分类类型
+        cat_type = pd.CategoricalDtype(categories=full_orderlist, ordered=True)
+        result['__cpd_align_sort'] = result[input_col].astype(cat_type)
+        
+        # 排序
+        result = result.sort_values(by='__cpd_align_sort', kind='mergesort')
         
         # 如果需要重命名列
         if output_col != input_col:
